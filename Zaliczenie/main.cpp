cc4f6de18167b45f3939d428b7d0513d508588bb
PTpwsz <doryg@interia.pl>
2020-05-21 17:08:57 +0200

    ZALICZENIE ZADANIA

---
+++ Zaliczenie/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <omp.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+    #pragma omp parallel num_threads(8)
+    {
+
+        int ID = omp_get_thread_num();
+        
+        cout << "Watek to: "<< ID << endl;
+        cout << "sdf";
+    }
+
+    //usleep(1000000);
+}
+
+
+
+
+//time.exe -f "czas trwania: %E" .\prog.exe
