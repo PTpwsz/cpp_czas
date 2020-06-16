0ea5179e4357538a53905720d7cf4debcfe9f2bf
PTpwsz <doryg@interia.pl>
2020-06-16 12:55:08 +0200

    test

--- Zaliczenie2/main.cpp
+++ Zaliczenie2/main.cpp
@@ -7,15 +7,7 @@
 
 using namespace std;
 
-void function()
-{   // funkcja wykonuje jakies bezsensowne, ale obciazajace obliczenia
-    double number { 0. };
-    #pragma omp parallel for reduction(+:number) num_threads(4)
-    for( long long i = 0; i != 200000000; ++i )
-    {
-       number += sin( static_cast<double>(i) / (i+1) );
-    }
-}
+
 
 int main()
 {
